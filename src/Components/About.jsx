979d4dcaf854e8da933b76e046dead3310e69db5
Shruti Sharma <alokshruti123@gmail.com>
2024-05-23 17:36:08 +0530

    initial commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,89 @@
+/**
+ * About component
+ *
+ * Space for you to describe more about yourself.
+ */
+
+import React from "react";
+
+/**
+ * About background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a image you
+ * freely use on your site.
+ */
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description =
+    "I am a 3rd-year B.E. Computer Science student at IET DAVV University in Indore, specializing in backend development. I am passionate about building robust and efficient server-side applications that ensure seamless performance and reliability.";
+
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
+const skillsList = [
+  "Programming Languages :",
+  "C++, Java, JavaScript",
+  "Database :",
+  "SQL, MongoDB",
+  "Frameworks :",
+  "Express.js, Node.js",
+  // "Focus group testing",
+  // "Mobile user interfaces",
+  // "Graphic design",
+];
+
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote =
+  "I am passionate about solving problems in new creative ways to drive innovation. I continually seeks new and better methods to make technology accessible to everyone. My  expertise in server-side development, allows me to create seamless and efficient digital experiences.";
+
+const About = () => {
+  return (
+    <section className="padding" id="about">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>About Myself</h2>
+        <p className="large">{description}</p>
+        <hr />
+        <ul
+          style={{
+            textAlign: "left",
+            columns: 2,
+            fontSize: "1.25rem",
+            margin: "2rem 3rem",
+            gap: "3rem",
+          }}
+        >
+          {skillsList.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
